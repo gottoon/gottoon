8b35f1fff6affa396b4f0101792f4720956fd77c
jin <bjkim.dev@gmail.com>
2014-07-21 10:30:23 +0900

    manager js 주석 제거

--- Integrated_project_ver5/WebContent/js/manager/manager.js
+++ Integrated_project_ver5/WebContent/js/manager/manager.js
@@ -4,25 +4,25 @@
 
 
 
-//$(document).ready(function() {
-//	function getAuthors() {
-//		var todo = "getAuthors";
-//		$.ajax({
-//			url : "/Integrated_project_ver4/action/manager",
-//			contentType : "application/x-www-form-urlencoded; charset=UTF-8",
-//			data : {
-//				todo : todo
-//			},
-//			success : function(data) {
-//				var authors = jQuery.parseJSON(data);
-//				$("#authors_name").autocomplete({
-//
-//					source : authors
-//				});
-//			}
-//		});
-//	}
-//
-//	var getAuthorsStart = getAuthors();
-//
-//});
+$(document).ready(function() {
+	function getAuthors() {
+		var todo = "getAuthors";
+		$.ajax({
+			url : "/Integrated_project_ver4/action/manager",
+			contentType : "application/x-www-form-urlencoded; charset=UTF-8",
+			data : {
+				todo : todo
+			},
+			success : function(data) {
+				var authors = jQuery.parseJSON(data);
+				$("#authors_name").autocomplete({
+
+					source : authors
+				});
+			}
+		});
+	}
+
+	var getAuthorsStart = getAuthors();
+
+});
